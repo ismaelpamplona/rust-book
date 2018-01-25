979f6cbcea6b9fba6e91b6a8c2d063982e2d2c20
Ismael Pamplona <ismaelpamplona@hotmail.com>
2018-01-24 22:27:30 -0200

    Guessing Game First Part
    Testing the First Part

---
+++ projects/guessing_game/target/debug/guessing_game.d
@@ -0,0 +1,1 @@
+/Users/IsmaelPamplona/google_drive/programacao/Workspace/rust-book/projects/guessing_game/target/debug/guessing_game: /Users/IsmaelPamplona/google_drive/programacao/Workspace/rust-book/projects/guessing_game/src/main.rs
